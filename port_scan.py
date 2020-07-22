016b71dc6c8e00a8c8e460840657c66211dc0f9e
James Maguire <neodreadlord@gmail.com>
2020-07-22 04:28:12 +0100

    Update port_scan.py

--- port_scan.py
+++ port_scan.py
@@ -6,53 +6,59 @@ from datetime import datetime
 print("-" * 60)
 print("PORT SCAN TARGET: " + sys.argv[1])
 print("-" * 60)
-#clearing screen
+
+# clearing screen
 subprocess.call('cls', shell = True)
 
-#taking supplied input of URL and retrieving sockets
+# taking supplied input of URL and retrieving sockets
+
 url = sys.argv[1]
-print (url)
+Punconverted = sys.argv[2]
+a, b = Punconverted.split('-')
+ps = int(a)
+pe = int(b)
+
 remoteServerIP = socket.gethostbyname(url)
 
 
-#banner
-print ("-" * 60)
-print ("Please wait, scanning remote host", remoteServerIP)
-print ("-" * 60)
+# banner
+print("-" * 60)
+print("Please wait, scanning remote host", remoteServerIP)
+print("-" * 60)
 
-#Check the time the scan started
+# Check the time the scan started
 time1 = datetime.now()
 
-#Using range function to specify ports. We are using (1-10), error handling also
+# Using range function to specify ports. We are using (1-10), error handling also
 try:
-    for port in range(79,82):
+    for port in range(ps, pe):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex((remoteServerIP, port))
 
         if result == 0:
-            print ("Port {}:    Open".format (port))
+            print("Port {}:    Open".format(port))
             sock.close()
         else:
-            print ("Port {}:    Closed".format(port))
+            print("Port {}:    Closed".format(port))
 
 except KeyboardInterrupt:
-    print ("Port {}:    Open".format(port))
-    print ("You cancelled the scan")
+    print("Port {}:    Open".format(port))
+    print("You cancelled the scan")
     sys.exit()
 
 except socket.gaierror:
-    print ("Hostname could not be resolved. Exiting scan")
+    print("Hostname could not be resolved. Exiting scan")
     sys.exit()
 
 except socket.error:
-    print ("Couldn't connect to server")
+    print("Couldn't connect to server")
     sys.exit()
 
-#Check time again
+# Check time again
 time2 = datetime.now()
 
-#Calculate time difference to workout run time
+# Calculate time difference to workout run time
 total_time = time2 - time1
 
-#print to screen
-print ("Scanning completed in: ", total_time)
+# print to screen
+print("Scanning completed in: ", total_time)
